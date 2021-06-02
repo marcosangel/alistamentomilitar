c7339ae62dc60effa24bc1420f760ad832bb8fcc
Willian dos Santos <williansantos465@hotmail.com>
2021-06-01 23:42:10 -0300

    Criando funções

--- alstamento.py
+++ alstamento.py
@@ -1,19 +1,30 @@
 from datetime import date
-sexo = int(input('Informe seu sexo:\n[1]-masculino\n[2]-feminino\nopção: '))
-if sexo == 1:
+
+def IdadeValida(i):
+    if(i >= 0):
+      return True
+def AnalisarIdadeParaAlistamento():
     atual = date.today().year
     nasc = int(input('Qual o ano de nascimento?: '))
+    idadeParaAlistamento = 18; 
     idade = atual - nasc
-    print('Quem nasceu em {}, tem {} anos em {}.'.format(nasc,idade,atual))
-    if idade == 18:
-        print('voce tem que se alistar IMEDIATAMENTE!')
-    elif idade < 18:
-        saldo = 18 - idade
-        print('Voce ainda não tem 18 anos. faltam {} anos para se alistar'.format(saldo))
-    elif idade > 18:
-        saldo = idade - 18
-        print('Voce ja deveria se alistado ha {} anos.'.format(saldo))
-elif sexo == 2:
-    print('Voce não precisa se alistar')
+    if(IdadeValida(idade)):            
+        print('Quem nasceu em {}, tem {} anos em {}.'.format(nasc,idade,atual))
+        if idade == idadeParaAlistamento:
+            print('voce tem que se alistar IMEDIATAMENTE!')
+        elif idade < idadeParaAlistamento:
+            saldo = idadeParaAlistamento - idade
+            print('Voce ainda não tem 18 anos. faltam {} anos para se alistar'.format(saldo))
+        elif idade > idadeParaAlistamento:
+            saldo = idade - idadeParaAlistamento
+            print('Voce ja deveria se alistado ha {} anos.'.format(saldo))
+    else:
+         print("A sua idade não é válida")        
 
-#Teste Willian
+#Inicio da análise 
+sexo = int(input('Informe o seu sexo:\n[1]masculino\n[2]feminino\nopção: '))        
+if(sexo == 1):
+    AnalisarIdadeParaAlistamento()
+else :    
+    print("Mulher não vale")
+
